0e19ec852c9c8a4fac869abcf4c6789d197952bf
yohns0201 <yohns0201@gmail.com>
2024-05-24 16:28:25 +0900

    전역컨트롤러 추가 및 코드 구조 최적화

--- src/main/java/com/example/demo/entity/ChatMessage.java
+++ src/main/java/com/example/demo/entity/ChatMessage.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
+
 import java.time.LocalDateTime;
 
 @Entity
--- src/main/java/com/example/demo/repository/ChatMessageRepository.java
+++ src/main/java/com/example/demo/repository/ChatMessageRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByUserIdOrderByTimestampAsc(String userId);
-
     List<ChatMessage> findByUserId(String userId);
 }
