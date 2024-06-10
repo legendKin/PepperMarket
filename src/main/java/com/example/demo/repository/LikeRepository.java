9936ad807e09bf39bc8264bb63f4c24698771161
bhher30 <happily850623@gmail.com>
2024-06-10 14:30:24 +0900

    좋아요 기능 추가 원래있던 코드들 병합수정

---
+++ src/main/java/com/example/demo/repository/LikeRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Like;
+import com.example.demo.entity.Board;
+import com.example.demo.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByBoardIdAndUserEmail(Integer boardId, String userEmail);
+    long countByBoardId(Integer boardId);
+}
+
