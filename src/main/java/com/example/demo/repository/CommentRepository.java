8f8f3d0b6b4f3bd4faeeb637fbf1d687758fc4c1
bhher30 <happily850623@gmail.com>
2024-06-12 10:10:08 +0900

    게시글 삭제시 채팅방도 삭제 , 판매완료된 게시글은 채팅못하게 기능 추가좀

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Comment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     List<Comment> findByBoardId(Integer boardId);
     Optional<Comment> findById(Integer commentId);
+    @Transactional
     void deleteByBoardId(Integer boardId);
+
 }
