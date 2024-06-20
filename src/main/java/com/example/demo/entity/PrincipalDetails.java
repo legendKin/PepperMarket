32fd328504c59b2d59f4c8298dd4367f59a42d4f
SHIN Yongsoo <yohns@kakao.com>
2024-06-21 02:00:45 +0900

    으아악

--- src/main/java/com/example/demo/entity/PrincipalDetails.java
+++ src/main/java/com/example/demo/entity/PrincipalDetails.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -94,5 +93,9 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     public String getProfilePic() {
         return users.getProfilePicPath();
+    }
+
+    public String getRole() {
+        return users.getRole().getAuthority();
     }
 }
